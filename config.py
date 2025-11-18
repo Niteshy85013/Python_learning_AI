e7f74bfbf9f779679f2065420004292fea48f9fe
Niteshy85013 <niteshyadav85013@gmail.com>
2025-11-18 02:18:00 +0000

    update

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('ITS_SECRET_KEY') or 'change-this-secret-key'
     # e.g. postgresql://username:password@localhost:5432/itsdb
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://postgres:Password@localhost:5432/ai_db'
+        'postgresql://postgres:Password@localhost:5432/tutor_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # Limits for submission evaluation (used in evaluator)
     CODE_EXECUTION_TIME = 3  # seconds allowed for restricted execution
